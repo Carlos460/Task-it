ea8acdb50a1a15836ff530e9e004afbc08ab35d6
Carlos Flores <carlosfloresalex@gmail.com>
2020-05-11 16:11:15 -0400

    added login route and fixed validation issues

--- source/tools/validation.js
+++ source/tools/validation.js
@@ -1,11 +1,13 @@
 const joi = require('@hapi/joi');
 
 //Validation from JOI
-const register = joi.object({
-    username: joi.string().min(3).max(30).required(),
-    email: joi.string().min(5).max(30).required().email(),
-    password: joi.string().min(5).required(),
-});
+const loginValidation = (data) => {
+    const schema = {
+        username: joi.string().min(3).max(30).required(),
+        password: joi.string().min(5).required(),
+    };
+    return joi.object(schema).validate(data);
+};
 
 const registerValidation = (data) => {
     const schema = {
@@ -17,3 +19,4 @@ const registerValidation = (data) => {
 };
 
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
