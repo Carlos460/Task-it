7dbfc969160f4dd08797d5ebf480caf89d732423
Carlos Flores <carlosfloresalex@gmail.com>
2020-05-10 20:53:32 -0400

    added js folder for frontend js, testing

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 //Import Express
-const express = require('express');
+const express = require("express");
 
 //Initializing Server and Port
 const server = express();
 const PORT = 8080;
 
 //Server Setup
-server.set('views', './source/views');
-server.set('view engine', 'ejs');
+server.set("views", "./source/views");
+server.set("view engine", "ejs");
 
-server.use(express.static('./source/public'));
+server.use(express.static("./source/public"));
 server.use(express.urlencoded({ extended: true }));
 
 //Route Imports
-let homepage = require('./source/routes/index.js');
+let homepage = require("./source/routes/index.js");
 
 //Using Route Imports
-server.use('/', homepage);
+server.use("/", homepage);
 
 //Server Listening to Requests!!!
 server.listen(PORT, () => {
-	console.log(`*** Server is running on: localhost:${PORT} ***`);
+  console.log(`*** Server is running on: localhost:${PORT} ***`);
 });
