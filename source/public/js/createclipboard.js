7ceb064f919dde932cb9d80f006572b4a1e63f8d
Carlos Flores <carlosfloresalex@gmail.com>
2020-05-31 15:24:28 -0400

    ajax requests changed to the fetch api, clipboardload.js file deleted

--- source/public/js/createclipboard.js
+++ source/public/js/createclipboard.js
@@ -1,16 +1,35 @@
 const createButton = document.querySelector('#create-button');
+const workspace = document.querySelector('.workspace');
 
+// Getting data from the database
 
-
-function createClipboard() {
-    let request = new XMLHttpRequest();
-
-    request.onreadystatechange = function () {
-        console.log(this.responseText);
-    };
-    request.open('POST', '/api/clipboard/add', true);
-    request.send();
+// clipboard element Template
+function clipboardElement(data) {
+    const clipboardHTML = `
+    <div>
+    <h1>Hello ${data}</h1>
+    </div>
+    `;
+    return clipboardHTML;
 };
 
+// Loadclipboard function
+function loadClipboard() {
+    const clipboardFrag = document
+        .createRange()
+        .createContextualFragment(clipboardElement("hellow world"));
+    // Apend clipboard to workspace in the DOM
+    workspace.appendChild(clipboardFrag);
+}
+
+
+// requests to create a clipboard
+async function createClipboard() {
+    const response = await fetch('http://localhost:8080/api/clipboard/add');
+    const data = await response.json();
+    console.log(data.message);
+}
 
 createButton.addEventListener('click', createClipboard);
+//Event listener for loading clipboard templates
+window.addEventListener('load', loadClipboard);
