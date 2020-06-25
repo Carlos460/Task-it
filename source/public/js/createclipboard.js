cdb6c9da1f5ac109b7dd8d9bce2bce858dfc44f9
Carlos Flores <carlosfloresalex@gmail.com>
2020-06-24 22:39:38 -0400

    primary color changed to orange, remove task button UI changed

--- source/public/js/createclipboard.js
+++ source/public/js/createclipboard.js
@@ -2,13 +2,14 @@ const createButton = document.querySelector('#create-button');
 const workspace = document.querySelector('.workspace');
 const titleElement = document.querySelector('.form-group');
 const titleInput = document.querySelector('#title-input');
+const flaskbutton = document.querySelector('.flask-button');
 
 // task template
 function taskElement(task) {
     let taskHTML = `
     <div class="task" unselectable="on">
         <div class="task-text">${task}</div>
-        <button class="delete" id="delete-task-button">X</button>  
+        <button class="delete delete-task-button">remove</button>  
     </div>`;
     return taskHTML;
 };
@@ -139,4 +140,46 @@ window.addEventListener('keypress', e => {
 
 window.addEventListener('load', (e) => {
     loadAllClipboards()
+});
+
+workspace.addEventListener('click', (e) => {
+    const targetTask = e.target;
+    let hasDoneClass = targetTask.classList.contains('done');
+    if (e.target.classList.contains('task-text')) {
+        if (hasDoneClass) {
+            targetTask.classList.remove('done');
+        } else if (!hasDoneClass) {
+            targetTask.classList.add('done');
+        }
+    }
+    if (e.target.classList.contains('delete')) {
+        const targetClipboardTitle = targetTask.parentElement.parentElement.parentElement.childNodes[1].childNodes[1].innerHTML;
+        const targetTaskText = targetTask.parentElement.childNodes[1].innerHTML;
+        removeTaskFromClipboard({
+            title: targetClipboardTitle,
+            taskText: targetTaskText
+        });
+        targetTask.parentElement.remove();
+    }
+    if (e.target.classList.contains('add-task')) {
+        const targetTasksContainer = targetTask.parentElement.parentElement.childNodes[3];
+        const targetClipboardTitle = targetTask.parentElement.parentElement.childNodes[1].childNodes[1].innerHTML;
+        const targetTaskInput = targetTask.parentElement.childNodes[3];
+        const taskData = {
+            clipboardTitle: targetClipboardTitle,
+            taskText: targetTaskInput.value
+        };
+        if (targetTaskInput.value !== '') {
+            addTaskToClipboard(taskData, targetTasksContainer);
+        }
+        targetTaskInput.value = '';
+    }
+    if (e.target.classList.contains('delete-clipboard-button')) {
+        const targetClipboardTitle = targetTask.parentElement.childNodes[1].innerHTML;
+        const clipboardData = {
+            title: targetClipboardTitle
+        }
+        removeClipboard(clipboardData);
+        targetTask.parentElement.parentElement.remove();
+    }
 });
